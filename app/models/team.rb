853578026ef00254408b4316c0011303cc766905
dddiehn <dddiehn@gmail.com>
2016-03-30 19:42:51 -0500

    initial team creation import from csv added

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,16 @@ class Team < ActiveRecord::Base
 
   validates :league, presence: true
   validates :name,   presence: true
+
+  def self.team_import(league_id, csv)
+    # http://stackoverflow.com/questions/4410794/ruby-on-rails-import-data-from-a-csv-file
+    require 'csv'
+    csv_text = File.read(csv)
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |row|
+      attributes = row.to_hash
+      attributes[:league_id] = league_id
+      Team.create!(attributes)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   def can?(verb, model)
     ability = Ability.new(self)
